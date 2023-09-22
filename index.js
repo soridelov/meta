a19c31ae64e2c987dc22cabe44f12e7ebed4444d
soridelov <145521073+soridelov@users.noreply.github.com>
2023-09-22 09:57:38 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,42 +1,16 @@
-// Importa las dependencias necesarias
 const express = require('express');
-const mongoose = require('mongoose');
-
-// Crea una instancia de la aplicación Express
 const app = express();
-const port = process.env.PORT || 3000; // Puerto en el que la aplicación escuchará
+const port = process.env.PORT || 3000;
 
-// Conecta con la base de datos (en este caso, MongoDB)
-mongoose.connect('mongodb://localhost/mi-basededatos', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// Configura Express para servir archivos estáticos desde el directorio 'public'
+app.use(express.static('public'));
 
-// Define un esquema de ejemplo para MongoDB (puedes personalizarlo)
-const tareaSchema = new mongoose.Schema({
-  nombre: String,
-  descripcion: String,
-});
-
-const Tarea = mongoose.model('Tarea', tareaSchema);
-
-// Configura rutas y controladores (endpoints)
+// Ruta principal que sirve el archivo HTML
 app.get('/', (req, res) => {
-  res.send('¡Hola, mundo!');
+  res.sendFile(__dirname + '/public/index.html');
 });
 
-app.get('/tareas', async (req, res) => {
-  // Consulta las tareas desde la base de datos y envía la respuesta
-  try {
-    const tareas = await Tarea.find();
-    res.json(tareas);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Error al obtener las tareas');
-  }
-});
-
-// Inicia el servidor
+// Escucha en el puerto especificado
 app.listen(port, () => {
   console.log(`Servidor escuchando en el puerto ${port}`);
 });
